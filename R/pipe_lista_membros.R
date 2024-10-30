21f1c37ed6582e8f8c5d9b896386ae433795ca40
arthurggnogueira <arthur@terravistacapital.com.br>
2024-10-30 14:12:37 -0300

    Ajuste função para listar membros da organização

--- R/pipe_lista_membros.R
+++ R/pipe_lista_membros.R
@@ -18,13 +18,7 @@ pipe_lista_membros <- function(header = c()){
 
   r <- pipefy::requisicao(query = query)
 
-  usuarios <- r$data$organization$members |>
-    purrr::discard(is.null) |>
-    unlist() |>
-    dplyr::bind_rows() |>
-    dplyr::select(email, id)
-
-  tibble_final <- purrr::map_dfr(r$data$organization$members, ~ tibble::as_tibble(.x[1]$user))
+  usuarios <- purrr::map_dfr(r$data$organization$members, ~ tibble::as_tibble(.x[1]$user))
 
   return(usuarios)
 }
